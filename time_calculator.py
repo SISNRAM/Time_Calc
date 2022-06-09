c6853b56903163e2a216e4334e4926067b6798c5
N_Ramzi <ramzii.noureddine@gmail.com>
2022-06-09 13:00:41 +0200

    First commit

--- time_calculator.py
+++ time_calculator.py
@@ -3,14 +3,13 @@ def add_time(start, end, day=""):
     day = day.casefold().title()
 
     # ****Define AM or PM******
-
+    x = ''
     period = ["AM", "PM"]
     timeS = str(start).split(":")
     S_hour = int(timeS[0])
     P = timeS[1].split()
     S_minutes = int(P[0])
     PeriodS = P[1]
-    print(PeriodS)
 
     timeE = str(end).split(":")
     E_hour = int(timeE[0])
@@ -18,17 +17,32 @@ def add_time(start, end, day=""):
 
     Res_H = S_hour + E_hour
     Res_Min = E_minutes + S_minutes
-
-    while Res_H > 12:
-        Res_H = Res_H - 12
+    cpt = 0
 
     while Res_Min > 59:
         Res_Min = Res_Min - 60
         Res_H += 1
 
+    while Res_H > 12:
+
+        Res_H = Res_H - 12
+        if PeriodS == 'PM':
+            PeriodS = 'AM,'
+
+        else:
+            PeriodS = 'PM,'
+
+        cpt = cpt + 1
+        if cpt == 1:
+            x = "(next day)"
+        if cpt >= 2:
+            x = "(" + str(cpt/2) + " Days Later)"
+
     NewTime = str(Res_H).zfill(2)+":"+str(Res_Min).zfill(2)
 
-    print(NewTime + + ",", day)
+    print(NewTime + " " + PeriodS + x, day)
 
 
-add_time("11:06 PM", "24:59", "moNday")
+add_time("6:30 PM", "205:12")
+
+# Error case 3 & 6
